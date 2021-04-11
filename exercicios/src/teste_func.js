b1bb88c6f8329664731ff79266b6906dc4579506
= <=>
2021-04-11 20:50:41 -0300

    Domingo - construct App functoion By function

---
+++ exercicios/src/teste_func.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+/*
+function App() {
+  return <SafeAreaView><Text>Hello</Text></SafeAreaView>;
+}
+*/
+const App = () => {
+  return (
+      <SafeAreaView>
+         <Text>Hello</Text>
+      </SafeAreaView>
+     
+  );
+};
+export default App;
